2b238108c91b5a6a5d19140a167a6d258139519c
hikmat farhat <hikmat.farhat@outlook.com>
2020-12-28 07:07:18 +0200

    added saxpy to example2

--- cuda-example2/cuda-example2.cu
+++ cuda-example2/cuda-example2.cu
@@ -1,33 +1,44 @@
 #include <iostream>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+// https://developer.nvidia.com/blog/six-ways-saxpy/
 
+__global__ void saxpy(int n, float a, float*  x, float*  y,float *z) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i < n)z[i] = a * x[i] + y[i];
+}
 
-__global__ void kernel(int* a, int* b, int* c) {
+__global__ void kernel(float* a, float* b, float* c) {
 	int idx = threadIdx.x;
 	c[idx] = a[idx] + b[idx];
 }
 
 int main() {
 	int N = 1024;
-	int* a, * b, * c;
-	int* da, * db, * dc;
-	a = (int*)malloc(N * sizeof(int));
-	b = (int*)malloc(N * sizeof(int));
-	c = (int*)malloc(N * sizeof(int));
+	float* a, * b, * c;
+	float* da, * db, * dc;
+	a = (float*)malloc(N * sizeof(float));
+	b = (float*)malloc(N * sizeof(float));
+	c = (float*)malloc(N * sizeof(float));
 
-	cudaMalloc(&da, N * sizeof(int));
-	cudaMalloc(&db, N * sizeof(int));
-	cudaMalloc(&dc, N * sizeof(int));
+	cudaMalloc(&da, N * sizeof(float));
+	cudaMalloc(&db, N * sizeof(float));
+	cudaMalloc(&dc, N * sizeof(float));
 	for (int i = 0; i < N; ++i) {
 		a[i] = i;
 		b[i] = 2 * i;
 	}
-	cudaMemcpy(da, a, N * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(db, b, N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(da, a, N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(db, b, N * sizeof(float), cudaMemcpyHostToDevice);
 
 	kernel << <1, N >> > (da, db, dc);
-	cudaMemcpy(c, dc, N * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(c, dc, N * sizeof(float), cudaMemcpyDeviceToHost);
+	for (int i = 0; i < 10; ++i)
+		std::cout << c[i] << ' ';
+	std::cout << std::endl;
+	saxpy << <4, 256 >> > (N, 3.5, da, db, dc);
+	cudaMemcpy(c, dc, N * sizeof(float), cudaMemcpyDeviceToHost);
+
 	for (int i = 0; i < 10; ++i)
 		std::cout << c[i] << ' ';
 	std::cout << std::endl;
